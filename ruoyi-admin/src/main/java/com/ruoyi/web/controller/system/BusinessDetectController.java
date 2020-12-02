3afb9bf73bb39354b2ee740740198ffef145241a
sd911223 <460475302@qq.com>
2020-12-02 09:56:46 +0800

    修改部门为部位

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/BusinessDetectController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/BusinessDetectController.java
@@ -71,37 +71,6 @@ public class BusinessDetectController extends BaseController {
     /**
      * 导出【商户检测】列表
      */
-
-    @GetMapping("/download")
-    public void download(@RequestParam("id") Long id, HttpServletResponse response) throws IOException {
-        try {
-            Resource resource = new ClassPathResource("static/poi/" + id + ".xlsx");
-            File file = resource.getFile();
-            String filename = resource.getFilename();
-            InputStream inputStream = new FileInputStream(file);
-            //强制下载不打开
-            response.setContentType("application/force-download");
-            OutputStream out = response.getOutputStream();
-            //使用URLEncoder来防止文件名乱码或者读取错误
-            response.setHeader("Content-Disposition", "attachment; filename=" + URLEncoder.encode(filename, "UTF-8"));
-            int b = 0;
-            byte[] buffer = new byte[1000000];
-            while (b != -1) {
-                b = inputStream.read(buffer);
-                if (b != -1) out.write(buffer, 0, b);
-            }
-            inputStream.close();
-            out.close();
-            out.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    /**
-     * 导出【商户检测】列表
-     */
     @RequiresPermissions("system:detect:export")
     @Log(title = "【商户检测】", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
